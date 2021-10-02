a496be003c6e5a47d1368a2f784afe2c76710863
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2021-10-02 10:15:10 -0300

    Melhora geração do arquivo

--- Program.cs
+++ Program.cs
@@ -1,24 +1,36 @@
-﻿Console.WriteLine("Iniciando gerador de labels para o Journal");
+﻿using JournalLabels;
+
+Console.WriteLine("Iniciando gerador de labels para o Journal");
 Console.WriteLine("Informe o ano desejado");
 var inputYear = Console.ReadLine();
 int year;
 
-if(!int.TryParse(inputYear, out year))
+if (!int.TryParse(inputYear, out year))
 {
     Console.WriteLine("Valor inválido");
     throw new Exception();
 }
+
 Console.WriteLine($"Gerando labels para o ano {year}");
 var filePath = $"C:\\Temp\\journal_labels_{year}.txt";
+if (File.Exists(filePath)) File.Delete(filePath);
 var sw = new StreamWriter(filePath);
 
-
+var currentMonth = 0;
 var date = new DateTime(year, 1, 1);
-while(date.Year == year)
+while (date.Year == year)
 {
+    if (date.Month != currentMonth)
+    {
+        var month = $"{date.ToString("MMMM").ToTitleCase()}";
+        Console.WriteLine(month);
+        sw.WriteLine(month);
+        currentMonth++;
+    }
+
     var dateTimePart = date.ToString("dd/MM/yyyy");
     var weekdayPart = date.ToString("ddd");
-    var journalLabel = $"{dateTimePart} {weekdayPart.Substring(0, 1).ToUpper()}{weekdayPart.Substring(1, 2).ToLower()}";
+    var journalLabel = $"{dateTimePart} {weekdayPart[..^1].ToTitleCase()}";
     Console.WriteLine(journalLabel);
     sw.WriteLine(journalLabel);
     date = date.AddDays(1);
---
+++ StringEx.cs
@@ -0,0 +1,12 @@
+﻿namespace JournalLabels;
+
+public static class StringEx
+{
+    public static string ToTitleCase(this string str)
+    {
+        if (str is null || str.Length < 2) throw new ArgumentException("String inválida");
+
+        var firstLetter = str[0].ToString().ToUpper();
+        return $"{firstLetter}{str[1..].ToLower()}";
+    }
+}
